7657fd1a5c058aa4bc2ca863985c80d313041786
shalexandeer <shalexander09@gmail.com>
2023-12-08 19:49:22 +0700

    Update completeThetriplets.js

--- completeThetriplets.js
+++ completeThetriplets.js
@@ -1,14 +1,9 @@
 let compareTriplets = (a, b) => {
     let [alice,bob] = [0,0];
-    let newArr = []
     for(let i = 0; i < a.length; i++){
         if(a[i] > b[i]) alice+=1;
         if(a[i] < b[i]) bob+=1;
-       
     }
-    newArr.push(alice)
-    newArr.push(bob)
-    return newArr
-
+    return `${alice} ${bob}`
 };
 console.log(compareTriplets([5,6, 7],[3,6, 10]));
